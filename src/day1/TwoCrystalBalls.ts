0b0bf927cd9f94dd99e2d6d020faaf6c764fcddc
Jakub Koci <jakub.koci@gmail.com>
2024-01-30 17:00:34 +0100

    Solve two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,14 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
 
+    let pointer = 0;
+    while (pointer < breaks.length) {
+        if (breaks[pointer]) {
+            for (let i = pointer - jump; i < pointer; i++) {
+                if (breaks[i]) return i;
+            }
+        }
+        pointer = pointer + jump;
+    }
+    return -1;
 }
