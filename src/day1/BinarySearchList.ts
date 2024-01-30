a3738f98e4c86cc2f82e66a0d774bf639e0881ae
Jakub Koci <jakub.koci@gmail.com>
2024-01-30 16:59:32 +0100

    Solve binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let start = 0;
+    let end = haystack.length;
 
+    while (start <= end) {
+        const middle = Math.floor(start + (end - start) / 2);
+        const value = haystack[middle];
+
+        if (value === needle) {
+            return true;
+        }
+        if (value < needle) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return false;
 }
