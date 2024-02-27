d66b1b6034dd342a7333ff6755f87e69debe3045
Jakub Koci <jakub.koci@gmail.com>
2024-02-27 15:43:36 +0100

    Solve doubly linked list

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,114 @@
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     prepend(item: T): void {
+        const node: Node<T> = { value: item };
+        if (!this.head) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+        this.length++;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx < 0) throw new Error("Out of range");
+        if (!this.head) throw new Error("List is empty");
+        const nodeAtPosition = this.getNodeAt(idx);
+        if (!nodeAtPosition) throw new Error("Out of range");
 
-}
+        const node: Node<T> = { value: item };
+        node.prev = nodeAtPosition.prev;
+        node.next = nodeAtPosition;
+
+        if (nodeAtPosition.prev) {
+            nodeAtPosition.prev.next = node;
+        } else {
+            this.head = node;
+        }
+
+        nodeAtPosition.prev = node;
+        this.length++;
+    }
+
     append(item: T): void {
+        const node: Node<T> = { value: item };
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let lastNode = this.head;
+            while (lastNode.next) {
+                lastNode = lastNode.next;
+            }
+            node.prev = lastNode;
+            lastNode.next = node;
+        }
+        this.length++;
+    }
 
-}
     remove(item: T): T | undefined {
+        let nodeWithValue = this.head;
+        while (nodeWithValue && nodeWithValue.value !== item) {
+            nodeWithValue = nodeWithValue.next;
+        }
+        if (nodeWithValue) {
+            this.removeNode(nodeWithValue);
+        }
+        return nodeWithValue?.value;
+    }
 
-}
     get(idx: number): T | undefined {
+        if (idx < 0) throw new Error("Out of range");
+        if (!this.head) throw new Error("List is empty");
+        const nodeAtPosition = this.getNodeAt(idx);
+        if (!nodeAtPosition) throw new Error("Out of range");
+        return nodeAtPosition.value;
+    }
 
-}
     removeAt(idx: number): T | undefined {
+        if (idx < 0) throw new Error("Out of range");
+        if (!this.head) throw new Error("List is empty");
+        let nodeAtPosition = this.getNodeAt(idx);
+        if (!nodeAtPosition) throw new Error("Out of range");
+        this.removeNode(nodeAtPosition);
+        return nodeAtPosition.value;
+    }
 
+    private getNodeAt(idx: number): Node<T> | undefined {
+        let nodeAtPosition;
+        if (this.head) {
+            nodeAtPosition = this.head;
+            for (let i = 0; i < idx; i++) {
+                if (!nodeAtPosition.next) break;
+                nodeAtPosition = nodeAtPosition.next;
+            }
+        }
+        return nodeAtPosition;
+    }
+
+    private removeNode(node: Node<T>) {
+        if (node.prev) {
+            node.prev.next = node.next;
+        } else {
+            this.head = node.next;
+        }
+
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+        this.length--;
+    }
 }
+
+interface Node<T> {
+    prev?: Node<T>;
+    next?: Node<T>;
+    value: T;
 }
