2ab4a13f52a93c35e48955c8a5cdbe782cd9cf21
Jakub Koci <jakub.koci@gmail.com>
2024-03-01 16:57:58 +0100

    Solve binary tree breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,11 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-
+    const queue: BinaryNode<number>[] = [];
+    queue.push(head);
+    while (queue.length > 0) {
+        const node = queue.pop();
+        if (node?.value === needle) return true;
+        if (node?.left) queue.push(node.left);
+        if (node?.right) queue.push(node.right);
+    }
+    return false;
 }
