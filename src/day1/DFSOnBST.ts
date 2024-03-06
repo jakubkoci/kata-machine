24e7ff04610ae4991b5fe228b293374bcc9d7a5e
Jakub Koci <jakub.koci@gmail.com>
2024-03-06 17:18:36 +0100

    Solve depth-first search on bst

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,23 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
+}
 
+function search(head: BinaryNode<number> | null, needle: number): boolean {
+    if (!head) {
+        return false;
+    }
+
+    if (needle === head.value) {
+        return true;
+    }
+
+    if (needle > head.value) {
+        return search(head.right, needle);
+    }
+
+    if (needle < head.value) {
+        return search(head.left, needle);
+    }
+
+    return false;
 }
