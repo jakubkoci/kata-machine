515ec60fceaee980693957820b7af9a3aee3a1ea
Jakub Koci <jakub.koci@gmail.com>
2024-03-06 15:35:15 +0100

    Solve compare binary tree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,17 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (a === null && b === null) {
+        return true;
+    }
 
+    if (a?.value === b?.value) {
+        const result = compare(a?.left || null, b?.left || null);
+        if (result) {
+            return compare(a?.right || null, b?.right || null);
+        }
+    }
+
+    return false;
 }
